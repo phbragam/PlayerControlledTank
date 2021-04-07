fdd311e7adf6bba70a6db6df0b57bc2e27118314
Pedro Braga <phbm.braga@gmail.com>
2021-04-07 07:08:18 -0300

    Initial Player Controlled Tank

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, translation, 0);
+
+        transform.Rotate(0, 0, -rotation);
+    }
+}
